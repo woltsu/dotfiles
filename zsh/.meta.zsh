d88a2ef660befe4224124694f6a157040e7865d6
Olli <olli.warro@gmail.com>
2023-12-17 13:20:40 +0200

    add clear alias

--- zsh/.meta.zsh
+++ zsh/.meta.zsh
@@ -1,5 +1,6 @@
 alias reload="source ~/.zshrc"
 alias zshrc="nano ~/.zshrc"
 alias dev="cd ~/Dev"
+alias c="clear"
 
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
